51b5b68f4ddedfa95bf53dd489e077db857a84ae
dreiissick <leojbautista30@gmail.com>
2025-04-07 00:49:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -47,11 +47,12 @@ const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2000,
-    delay: 200,
+    delay: 500,
 //     reset: true
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 700}); 
+sr.reveal('.home__social-icon',{ interval: 300}); 
+sr.reveal('.skills__data, .hobbies__img, .contact__input',{interval: 200}); 
+
